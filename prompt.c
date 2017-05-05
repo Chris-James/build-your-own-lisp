4c6bc20b48a1c035368d0ac93752b9429a9667b5
Chris James <chris@chris-james.me>
2017-05-05 07:56:04 -0400

    prompt.c: Add Windows preprocessor directives

--- prompt.c
+++ prompt.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+#ifdef _WIN32
+
+#include <string.h>
+#define BUFF_SIZE 2048
+
+static char buffer[BUFF_SIZE];
+
+char * readline(char * prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, BUFF_SIZE, stdin);
+  char * cpy = malloc(strlen(buffer) + 1)
+  strcpy(cpy, buffer);
+  cpy[(strlen(cpy) - 1)] = '\0';
+  return cpy;
+}
+
+void add_history(char * unused) {}
+#else
 #include <editline/readline.h>
+#endif
 
 int main(int argc, char ** argv) {
 
