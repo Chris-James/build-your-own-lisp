3b0afe61644567d13b0bd601810a174b0a7951f1
Chris James <chris@chris-james.me>
2017-05-06 14:10:00 -0400

    prompt.c/lispy: Add % operator

--- prompt.c
+++ prompt.c
@@ -35,7 +35,7 @@ int main(int argc, char ** argv) {
 
   mpca_lang(MPCA_LANG_DEFAULT,
     " number  : /-?[0-9]+(\\.[0-9]+)?/;                \
-      operator: '+' | '-' | '*' | '/';                 \
+      operator: '+' | '-' | '*' | '/' | '%';           \
       expr    : <number> | '(' <operator> <expr>+ ')'; \
       lispy   : /^/ <operator> <expr>+ /$/;            \
     ",
@@ -95,6 +95,9 @@ long eval_op(long x, char* op, long y) {
       break;
     case '/':
       result = x / y;
+      break;
+    case '%':
+      result = x % y;
       break;
   }
 
