2e8088b7a0cd7568264cd596d5500e43c950e918
Chris James <chris@chris-james.me>
2017-05-09 12:11:17 -0400

    prompt.c/lispy: Change 'operator' to 'symbol' in grammar

--- prompt.c
+++ prompt.c
@@ -70,17 +70,17 @@ lval eval(mpc_ast_t* t);
 int main(int argc, char ** argv) {
 
   mpc_parser_t * Number = mpc_new("number");
-  mpc_parser_t * Operator = mpc_new("operator");
+  mpc_parser_t * Symbol = mpc_new("symbol");
   mpc_parser_t * Expr = mpc_new("expr");
   mpc_parser_t * Lispy = mpc_new("lispy");
 
   mpca_lang(MPCA_LANG_DEFAULT,
     " number  : /-?[0-9]+(\\.[0-9]+)?/;                             \
-      operator: '+' | '-' | '*' | '/' | '%' | '^' | /m((in)|(ax))/; \
-      expr    : <number> | '(' <operator> <expr>+ ')';              \
-      lispy   : /^/ <operator> <expr>+ /$/;                         \
+      symbol: '+' | '-' | '*' | '/' | '%' | '^' | /m((in)|(ax))/; \
+      expr    : <number> | '(' <symbol> <expr>+ ')';              \
+      lispy   : /^/ <symbol> <expr>+ /$/;                         \
     ",
-    Number, Operator, Expr, Lispy
+    Number, Symbol, Expr, Lispy
   );
 
   puts("Lispy Version 0.0.0.0.1");
@@ -107,7 +107,7 @@ int main(int argc, char ** argv) {
     free(input);
   }
 
-  mpc_cleanup(4, Number, Operator, Expr, Lispy);
+  mpc_cleanup(4, Number, Symbol, Expr, Lispy);
   return 0;
 }
 
