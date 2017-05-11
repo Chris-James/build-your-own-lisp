399a755078f5435ae0e4195b7e21459a58c805b1
Chris James <chris@chris-james.me>
2017-05-11 17:36:47 -0400

    prompt.c: Update function prototypes

--- prompt.c
+++ prompt.c
@@ -68,11 +68,8 @@ lval* make_lval(int type, value x);
 void lval_del(lval* v);
 lval* lval_add(lval* s_expr, lval* new_lval);
 lval* lval_read(mpc_ast_t* t);
-
-void lval_print(lval* v);
-void lval_println(lval v);
-
-lval eval(mpc_ast_t* t);
+void lval_println(lval* v);
+lval* lval_eval(lval* v);
 
 int main(int argc, char ** argv) {
 
