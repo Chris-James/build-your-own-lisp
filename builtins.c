13259af8aae19293dfb5dd019fda50bb77d6f2dd
Chris James <chris@chris-james.me>
2017-05-15 22:26:37 -0400

    builtins.c/builtin_eval(): Fix comments

--- builtins.c
+++ builtins.c
@@ -259,10 +259,10 @@ lval* builtin_list(lval* a) {
  */
 lval* builtin_eval(lval* a) {
 
-  // Ensure `len` was passed only one argument
+  // Ensure only one argument passed
   L_ASSERT(args, args->count == 1, L_ERR_ARG_COUNT);
 
-  // Ensure `len` was passed a Q-Expression
+  // Ensure argument passed was a Q-Expression
   L_ASSERT(args, args->val.cell[0]->type == LVAL_QEXPR, L_ERR_BAD_TYPE);
 
   lval* x = lval_take(a, 0);
